f6a619975d3a63d2f5c65c04d1c0934408cad05c
Tribanco Dev <tribancodev@Mac-mini-de-Tribanco.local>
2018-08-01 13:33:43 -0300

    Inicio Projeto

---
+++ MinhasAnotacoes/MinhasAnotacoes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MinhasAnotacoes
+//
+//  Created by Tribanco Dev on 01/08/2018.
+//  Copyright © 2018 minhasAnotacoes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
