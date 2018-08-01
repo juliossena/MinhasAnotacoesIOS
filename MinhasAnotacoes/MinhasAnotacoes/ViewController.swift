62f416c6b2fb282cb5b8b424af759b4b82251ee9
Tribanco Dev <tribancodev@Mac-mini-de-Tribanco.local>
2018-08-01 15:01:00 -0300

    Final do projeto

--- MinhasAnotacoes/MinhasAnotacoes/ViewController.swift
+++ MinhasAnotacoes/MinhasAnotacoes/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let chave :String = "MinhasAnotacoes"
+    
+    @IBOutlet weak var texto: UITextView!
+    @IBAction func salvarDados(_ sender: Any) {
+        let texto :String = self.texto.text
+        UserDefaults.standard.set(texto, forKey: chave)
+        
+        
+    }
+    
+    func carregarDados () -> String{
+        if let texto :String = UserDefaults.standard.string(forKey: chave) {
+            return texto
+        }
+        return ""
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.texto.text = carregarDados()
     }
 
     override func didReceiveMemoryWarning() {
